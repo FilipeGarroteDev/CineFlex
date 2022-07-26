27e170fbd6fbb65b3904eabd9a8853d2ce3d119d
Filipe Garrote <filipe.garrote@gmail.com>
2022-07-26 15:47:38 -0300

    Add reset.css e instalação do Router

---
+++ src/Components/App/App.js
@@ -0,0 +1,12 @@
+import "../../assets/css/reset.css"
+import "./style.css"
+
+export default function App(){
+  return (
+    <>
+      <header>
+        <h1>CINEFLEX</h1>
+      </header>
+    </>
+  )
+}

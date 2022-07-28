84e7849b794904606c85b5454235b4bb599eb4f8
Filipe Garrote <filipe.garrote@gmail.com>
2022-07-28 01:08:02 -0300

    v5.0 - Add Layout da Página de assentos (SeatsPage)

--- src/Components/SeatsPage/SeatsPage.js
+++ src/Components/SeatsPage/SeatsPage.js
@@ -6,7 +6,7 @@ import React from "react"
 
 export default function MovieSessions(){
 
-  let x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+  let x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
 
 
   return (<>
@@ -21,6 +21,20 @@ export default function MovieSessions(){
           </>)}
       </ul>
       
+      <label className="seatsSubtitle">
+        <div className="option">
+          <div className="seat selected"></div>
+          <span>Selecionado</span>
+        </div>
+        <div className="option">
+          <div className="seat"></div>
+          <span>Disponível</span>
+        </div>
+        <div className="option">
+          <div className="seat unavaiable"></div>
+          <span>Indisponível</span>
+        </div>
+      </label>
     </div>
     <Footer />
   </>)
